461f369a565f3754d0854915b6bcfc3b85f5a13e
eric.vautour <eric.vautour@kineticsocial.com>
2014-01-29 16:44:55 -0500

    TASK KSP-1735: New object is in place with tests

--- spec/fb_graph/ad_campaign_group_spec.rb
+++ spec/fb_graph/ad_campaign_group_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe FbGraph::AdCampaignGroup, '.new' do
   it 'should setup all supported attributes' do
     attributes = {
-      :id => '6003266501233',
+      :id => '6015568499638',
       :account_id => 12345566,
       :name => "Test Ad Campaign Group",
       :objective => 'PAGE_LIKES',
@@ -11,7 +11,7 @@ describe FbGraph::AdCampaignGroup, '.new' do
       :is_completed => 0
     }
     ad_campaign_group = FbGraph::AdCampaignGroup.new(attributes.delete(:id), attributes)
-    ad_campaign_group.identifier.should == "6003266501233"
+    ad_campaign_group.identifier.should == "6015568499638"
     ad_campaign_group.account_id.should == 12345566
     ad_campaign_group.name.should == 'Test Ad Campaign Group'
     ad_campaign_group.objective.should == 'PAGE_LIKES'
@@ -22,10 +22,10 @@ end
 
 describe FbGraph::AdCampaignGroup, '.fetch' do
   it 'should get the ad campaign group' do
-    mock_graph :get, '6003266501233', 'ad_campaign_groups/test_ad_campaign_group', :access_token => 'access_token' do
-      ad_campaign_group = FbGraph::AdCampaignGroup.fetch('6003266501233', :access_token => 'access_token')
+    mock_graph :get, '6015568499638', 'ad_campaign_groups/test_ad_campaign_group', :access_token => 'access_token' do
+      ad_campaign_group = FbGraph::AdCampaignGroup.fetch('6015568499638', :access_token => 'access_token')
 
-      ad_campaign_group.identifier.should == "6003266501233"
+      ad_campaign_group.identifier.should == "6015568499638"
       ad_campaign_group.account_id.should == 12345566
       ad_campaign_group.name.should == "Test Ad Campaign"
       ad_campaign_group.objective.should == 'PAGE_LIKES'
--- spec/fb_graph/connections/ad_campaigns_spec.rb
+++ spec/fb_graph/connections/ad_campaigns_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 describe FbGraph::Connections::AdCampaigns, '#ad_campaign!' do
+  context 'when included by FbGraph::AdCampaignGroup' do
+    context 'when access_token is given' do
+      it 'should return ad_campaigns as FbGraph::AdCampaign' do
+        mock_graph :get, '6015568499638/adcampaigns', 'ad_campaign_groups/ad_campaigns/6015568499638_ad_campaigns', :access_token => 'access_token' do
+          ad_campaigns = FbGraph::AdCampaignGroup.new('6015568499638', :access_token => 'access_token').ad_campaigns
+          ad_campaigns.each { |ad_campaign| ad_campaign.should be_instance_of(FbGraph::AdCampaign) }
+          ad_campaigns.first.should == FbGraph::AdCampaign.new(
+            "6015568577438",
+            :access_token => 'access_token',
+            :name => "(15423) Test New Structure 1"
+          )
+        end
+      end
+    end
+  end
+  
   context 'when included by FbGraph::AdAccount' do
     it 'should return generated ad_account' do
       mock_graph :post, 'act_22334455/adcampaigns', 'ad_accounts/ad_campaigns/post_with_valid_access_token' do
