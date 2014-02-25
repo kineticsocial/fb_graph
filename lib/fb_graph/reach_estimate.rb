c2c639ef0814dbabf15983f8b2764e0bea8198dd
Paul Mason <paul.mason@kineticsocial.com>
2014-02-25 11:42:51 -0500

    KSP-1550: Adding cpa to reach estimate

--- lib/fb_graph/reach_estimate.rb
+++ lib/fb_graph/reach_estimate.rb
@@ -1,7 +1,7 @@
 module FbGraph
   class ReachEstimate < Node
 
-    attr_accessor :users, :cpc_max, :cpc_median, :cpc_min, :cpm_max, :cpm_median, :cpm_min
+    attr_accessor :users, :cpc_max, :cpc_median, :cpc_min, :cpm_max, :cpm_median, :cpm_min, :cpa_max, :cpa_median, :cpa_min
 
     def initialize(attributes = {})
       super
@@ -13,7 +13,7 @@ module FbGraph
         send("#{field}=", attributes[field.to_sym])
       end
 
-      %w(cpc_min cpc_median cpc_max cpm_min cpm_median cpm_max).each do |field|
+      %w(cpc_min cpc_median cpc_max cpm_min cpm_median cpm_max cpa_min cpa_median cpa_max).each do |field|
         send("#{field}=", attributes[:bid_estimations][0][field.to_sym])
       end
     end
