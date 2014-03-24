1f7806211379acfa9d109a497dd6a0021cd165b3
Kevin Wang <kevin.wang@kineticsocial.com>
2014-03-24 16:44:54 -0400

    KSP-1889: renamed an internal param key of FbGraph::Searchable module to avoid a conflict with FB Graph APIs; enabled FbGraph::AdTargetingCategory class.

--- lib/fb_graph/searchable.rb
+++ lib/fb_graph/searchable.rb
@@ -1,18 +1,18 @@
 module FbGraph
   module Searchable
     def self.search(query, options = {})
-      klass = options.delete(:class) || Searchable
-      query_param = klass.search_query_param
+      self_class = options.delete(:self_class) || Searchable
+      query_param = self_class.search_query_param
       collection = Collection.new(
         Node.new(:search).send(:get, options.merge(query_param.to_sym => query))
       )
       yield collection if block_given?
-      Searchable::Result.new(query, klass, options.merge(:collection => collection))
+      Searchable::Result.new(query, self_class, options.merge(:collection => collection))
     end
 
     def search(query, options = {})
       type = self.to_s.underscore.split('/').last.gsub("_", "")
-      Searchable.search(query, options.merge(:type => type, :class => self)) do |collection|
+      Searchable.search(query, options.merge(:type => type, :self_class => self)) do |collection|
         collection.map! do |obj|
           self.new(obj[:id], obj.merge(
             :access_token => options[:access_token]
