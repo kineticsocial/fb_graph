9b2dffc947b538432e1bfc715b481f083d8dd44e
Kevin Wang <kevin.wang@kineticsocial.com>
2014-07-29 11:32:31 -0400

    Add BusinessManager endpoint

---
+++ lib/fb_graph/business_manager.rb
@@ -0,0 +1,27 @@
+module FbGraph
+  class BusinessManager < Node
+    include Connections::AdAccounts
+
+    ATTRS = [
+      :id,
+      :name,
+      :timezone_id,
+      :primary_page,
+      :vertical_id,
+      :update_time,
+      :updated_by,
+      :creation_time,
+      :created_by
+    ]
+
+    attr_accessor *ATTRS
+
+    def initialize(identifier, attributes = {})
+      super
+
+      ATTRS.each do |field|
+        send("#{field}=", attributes[field.to_sym])
+      end
+    end
+  end
+end
