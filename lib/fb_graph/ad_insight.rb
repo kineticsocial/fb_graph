3cb63c7700ee59de6b6797177d9e7b7b10eabe48
Eric Vautour <eric.vautour@kineticsocial.com>
2015-05-25 14:28:19 -0400

    Added definitions

--- lib/fb_graph/ad_insight.rb
+++ lib/fb_graph/ad_insight.rb
@@ -2,84 +2,102 @@ module FbGraph
   class AdInsight < Node
     include Connections::AdInsights
     
-    DIMENSION_COLUMNS = [
-      :account_id, #The ad account id
-      :account_name, #The ad account name
-      :adgroup_id, #The ad group id
-      :adgroup_name, #The ad group name
-      :campaign_id, #The campaign id
-      :campaign_name, #The campaign name
-      :campaign_group_id, #The campaign group id
-      :campaign_group_name #The campaign group name
-    ]
-
+    #
+    # Data Break downs
+    #
     DATA_BREAKDOWN_COLUMNS = [
-      # Demographic
-      :age, # The age range of the people you've reached
-      :country, #T he countries where the people reached are located
-      :gender, # Gender of the people reached
-      :placement, # Where ad was shown on Facebook, such as on News Feed on a mobile device or on the right-side of Facebook on a desktop computer
-      :impression_device # NOT a real breakdown column, but acts as one with placement
-    ]
-
-    DATA_ACTION_COLUMNS = [
-      :action_values, 
-      :actions, 
-      :actions_per_impression,
-      :unique_actions
+      :age,
+      :country,
+      :gender,
+      :placement,
+      :impression_device
     ]
     
-    VIDEO_ACTION_COLUMNS = [
-      :video_avg_pct_watched_actions, 
-      :video_avg_sec_watched_actions, 
-      :video_complete_watched_actions, 
-      :video_p25_watched_actions, 
-      :video_p50_watched_actions, 
-      :video_p75_watched_actions, 
-      :video_p95_watched_actions, 
-      :video_p100_watched_actions, 
-      :video_start_actions
-    ]
+    #
+    # Dimensions
+    #
+    DIMENSION_COLUMN_TYPES = {
+      :account_id => {:type => :integer}, # The ad account id
+      :account_name => {:type => :string}, # The ad account name
+      :adgroup_id => {:type => :integer}, # The ad group id
+      :adgroup_name => {:type => :string}, # The ad group name
+      :campaign_id => {:type => :integer}, # The campaign id
+      :campaign_name => {:type => :string}, # The campaign name
+      :campaign_group_id => {:type => :integer}, # The campaign group id
+      :campaign_group_name => {:type => :string} #The campaign group name
+    }
+    DIMENSION_COLUMNS = DIMENSION_COLUMN_TYPES.keys
     
-    DATA_COLUMNS = [
-      :deeplink_clicks, 
-      :app_store_clicks, 
-      :call_to_action_clicks, 
-      :clicks, 
-      :unique_clicks, 
-      :cost_per_action_type, 
-      :cost_per_result, 
-      :cost_per_total_action, 
-      :cpc, 
-      :cost_per_unique_click, 
-      :cpm, 
-      :cpp, 
-      :ctr, 
-      :unique_ctr, 
-      :frequency, 
-      :impressions, 
-      :unique_impressions, 
-      :objective, 
-      :performance_indicator, 
-      :product_id, 
-      :reach, 
-      :result_rate, 
-      :results, 
-      :relevance_score, 
-      :roas, 
-      :social_clicks, 
-      :unique_social_clicks, 
-      :social_impressions, 
-      :unique_social_impressions, 
-      :social_reach, 
-      :spend, 
-      :today_spend, 
-      :total_action_value, 
-      :total_actions, 
-      :total_unique_actions, 
-      :website_clicks, 
-      :website_ctr
-    ]
+    #
+    # Actions
+    #
+    DATA_ACTION_TYPES = {
+      :action_values => {:type => :object, :sub_type => :decimal},
+      :actions => {:type => :object, :sub_type => :integer}, 
+      :unique_actions => {:type => :object, :sub_type => :integer},
+      :cost_per_action_type => {:type => :object, :sub_type => :decimal}
+    }
+    DATA_ACTION_COLUMNS = DATA_ACTION_TYPES.keys
+    
+    #
+    # Video
+    #
+    VIDEO_ACTION_COLUMN_TYPES = {
+      :video_avg_pct_watched_actions => {:type => :object, :sub_type => :percent},
+      :video_avg_sec_watched_actions => {:type => :object, :sub_type => :integer},
+      :video_complete_watched_actions => {:type => :object, :sub_type => :integer},
+      :video_p25_watched_actions => {:type => :object, :sub_type => :integer}, 
+      :video_p50_watched_actions => {:type => :object, :sub_type => :integer}, 
+      :video_p75_watched_actions => {:type => :object, :sub_type => :integer}, 
+      :video_p95_watched_actions => {:type => :object, :sub_type => :integer}, 
+      :video_p100_watched_actions => {:type => :object, :sub_type => :integer}, 
+      :video_start_actions => {:type => :object, :sub_type => :integer}
+    }
+    VIDEO_ACTION_COLUMNS = VIDEO_ACTION_COLUMN_TYPES.keys
+    
+    #
+    # General Fields
+    #
+    DATA_COLUMN_TYPES = {
+      :actions_per_impression => {:type => :int},
+      :deeplink_clicks => {:type => :int},
+      :app_store_clicks => {:type => :int},
+      :call_to_action_clicks => {:type => :int},
+      :clicks => {:type => :int},
+      :unique_clicks => {:type => :int},
+      :cost_per_result => {:type => :decimal},
+      :cost_per_total_action => {:type => :decimal},
+      :cpc => {:type => :decimal},
+      :cost_per_unique_click => {:type => :decimal},
+      :cpm => {:type => :decimal},
+      :cpp => {:type => :decimal},
+      :ctr => {:type => :decimal},
+      :unique_ctr => {:type => :decimal},
+      :frequency => {:type => :decimal},
+      :impressions => {:type => :int},
+      :unique_impressions => {:type => :int},
+      :objective => {:type => :string},
+      :performance_indicator => {:type => :string},
+      :reach => {:type => :int},
+      :result_rate => {:type => :decimal},
+      :results => {:type => :int},
+      :relevance_score => {:type => :object, :sub_type => :string},
+      :roas => {:type => :decimal},
+      :social_clicks => {:type => :int},
+      :unique_social_clicks => {:type => :int},
+      :social_impressions => {:type => :int},
+      :unique_social_impressions => {:type => :int},
+      :social_reach => {:type => :int},
+      :spend => {:type => :decimal},
+      :today_spend => {:type => :decimal},
+      :total_action_value => {:type => :decimal},
+      :total_actions => {:type => :int},
+      :total_unique_actions => {:type => :int},
+      :website_clicks => {:type => :int},
+      :website_ctr => {:type => :int},
+      :product_id => {:type => :int}
+    }
+    DATA_COLUMNS = DATA_COLUMN_TYPES.keys
 
     # Dimension + Column for Action reporting
     # Note that the 'spend' field in 'data_columns' does not work with action breakdowns
